8bc41b22508a57cace1da3df1edeba6650382396
Petersap4 <97790300+Petersap4@users.noreply.github.com>
2025-10-28 17:20:13 +0200

    Add files via upload

---
+++ SpecialMatrix.java
@@ -0,0 +1,43 @@
+import java.lang.Math;
+public class SpecialMatrix {
+	int dim;
+	int[][] matrix; 
+	SpecialMatrix(int dim)
+	{
+		this.dim = dim;
+		matrix = new int[dim][dim];
+	}
+	
+	void createSpecialMatrix()
+	{
+		for(int i = 0; i<dim; i++)
+		{
+			for(int j = 0; j<dim; j++)
+				matrix[i][j] =(int)(Math.random() *2);
+		}
+	}
+	
+	int countOnes()
+	{
+		int cnt = 0;
+		for(int i=0; i<Math.pow(dim, 2); i++)
+		{
+			if(matrix[i/dim][i%dim] == 1)
+				cnt++;
+		}		
+		return cnt;
+	}
+		
+	public static void main(String[] args) {
+		SpecialMatrix obj = new SpecialMatrix(10);
+		obj.createSpecialMatrix();
+		for(int i=0; i<10; i++)
+		{
+			for(int j=0; j<10; j++)
+				System.out.print(obj.matrix[i][j] + " ");
+			System.out.println();
+		}
+		System.out.println("Count of ones: " + obj.countOnes());
+	}
+
+}
